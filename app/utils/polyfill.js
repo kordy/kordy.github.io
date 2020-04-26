77769d4ff01cabdfd3bb20aab7e07218c8bc1ba6
Dmitrii <dimawork.spb@gmail.com>
2020-04-27 00:31:35 +0300

    polyfill let to var

--- app/utils/polyfill.js
+++ app/utils/polyfill.js
@@ -9,7 +9,7 @@
   // closest
   if (!Element.prototype.closest) {
     Element.prototype.closest = function(css) {
-      let node = this;
+      var node = this;
 
       while (node) {
         if (node.matches(css)) return node;
